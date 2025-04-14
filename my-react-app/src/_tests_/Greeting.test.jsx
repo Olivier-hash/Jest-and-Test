f5fe1a5bb704d7492d224f79266145169b3fafe1
olivier Rukundo <rr.olivier01@gmail.com>
2025-04-14 21:08:37 +0200

    Fixed displaying of the component Greeting from the App.jsx

---
+++ my-react-app/src/_tests_/Greeting.test.jsx
@@ -0,0 +1,9 @@
+// _tests_Greeting.test.jsx
+import { render, screen } from "@testing-library/react";
+import Greeting from "../components/Greeting";
+
+test("renders greeting with the correct name", () => {
+  render(<Greeting name="Olivier" />);
+  const greetingText = screen.getByText("Hello, Olivier!");
+  expect(greetingText).toBeInTheDocument();
+});import React from "react";
